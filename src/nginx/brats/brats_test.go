8820752448fa0ec8f8dd6952892b0b134e97e7ec
Dave Goddard <dave@goddard.id.au>
2018-01-25 16:01:30 -0500

    Add BRATs tests [#154646197]
    Signed-off-by: Jack Feeny <jfeeny@pivotal.io>

---
+++ src/nginx/brats/brats_test.go
@@ -0,0 +1,26 @@
+package brats_test
+
+import (
+	"github.com/cloudfoundry/libbuildpack/bratshelper"
+	"github.com/cloudfoundry/libbuildpack/cutlass"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Nginx buildpack", func() {
+	bratshelper.UnbuiltBuildpack("nginx", CopyBrats)
+	bratshelper.DeployingAnAppWithAnUpdatedVersionOfTheSameBuildpack(CopyBrats)
+	bratshelper.StagingWithADepThatIsNotTheLatest("nginx", CopyBrats)
+	bratshelper.DeployAppWithExecutableProfileScript("nginx", CopyBrats)
+	bratshelper.DeployAnAppWithSensitiveEnvironmentVariables(CopyBrats)
+	bratshelper.ForAllSupportedVersions("nginx", CopyBrats, func(nginxVersion string, app *cutlass.App) {
+		bratshelper.PushApp(app)
+
+		By("installs the correct version of Nginx", func() {
+			Expect(app.Stdout.String()).To(ContainSubstring("Installing nginx " + nginxVersion))
+		})
+		By("runs a simple webserver", func() {
+			Expect(app.GetBody("/")).To(ContainSubstring("Hello World!"))
+		})
+	})
+})
