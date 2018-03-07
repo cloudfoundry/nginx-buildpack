6e78ef01336005265115beb5b37fcb50be114b3d
Dave Goddard <dave@goddard.id.au>
2018-03-07 11:08:17 -0400

    Run both cached and uncached integration tests

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export ROOT=$(dirname $(readlink -f ${BASH_SOURCE%/*}))
-$ROOT/scripts/install_tools.sh
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+source .envrc
+./scripts/install_tools.sh
 
 GINKGO_NODES=${GINKGO_NODES:-3}
 GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-1}
 
-cd $ROOT/src/nginx/integration
-ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES
+cd src/*/integration
+
+echo "Run Uncached Buildpack"
+ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=120 --noisySkippings=false -- --cached=false
+
+echo "Run Cached Buildpack"
+ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=120 --noisySkippings=false -- --cached
