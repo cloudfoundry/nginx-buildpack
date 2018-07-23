ffc5919e67443e14354ff61e095ef76ce284508c
Tyler Phelan <tphelan@pivotal.io>
2018-07-23 16:12:01 -0400

    Handle testing with different stacks
    [#144940817]

--- scripts/brats.sh
+++ scripts/brats.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export ROOT=$(dirname $(readlink -f ${BASH_SOURCE%/*}))
+export ROOT="$( dirname "${BASH_SOURCE[0]}" )/.."
 $ROOT/scripts/install_tools.sh
 
 GINKGO_NODES=${GINKGO_NODES:-3}
--- src/nginx/integration/push_app_second_time_test.go
+++ src/nginx/integration/push_app_second_time_test.go
@@ -26,8 +26,9 @@ var _ = Describe("pushing an app a second time", func() {
 		app = cutlass.New(filepath.Join(bpDir, "fixtures", "mainline"))
 	})
 
-	DownloadRegexp := `Download \[.*/nginx\-[\d\.]+\-linux-x64-[0-9a-f]+\.tgz\]`
-	CopyRegexp := `Copy \[.*/nginx\-[\d\.]+\-linux-x64-[0-9a-f]+\.tgz\]`
+	Regexp := `\[.*/nginx\-[\d\.]+\-linux\-x64\-(cflinuxfs.*-)?[\da-f]+\.tgz\]`
+	DownloadRegexp := "Download " + Regexp
+	CopyRegexp := "Copy " + Regexp
 
 	It("uses the cache for manifest dependencies", func() {
 		PushAppAndConfirm(app)
