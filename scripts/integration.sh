be51246951572684043644fe5ac278c642e80693
Tyler Phelan <tphelan@pivotal.io>
2018-12-03 15:17:26 -0500

    Allow integration test scripts to pass in buildpack zips
    - Pass in file paths as env vars
    - paths default to  for typical behavior
    
    Co-authored-by: Zander Mackie <amackie@pivotal.io>

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -6,7 +6,7 @@ source .envrc
 ./scripts/install_tools.sh
 
 GINKGO_NODES=${GINKGO_NODES:-3}
-GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-1}
+GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-2}
 export CF_STACK=${CF_STACK:-cflinuxfs3}
 
 UNCACHED_BUILDPACK_FILE=${UNCACHED_BUILDPACK_FILE:-""}
@@ -15,9 +15,9 @@ CACHED_BUILDPACK_FILE=${CACHED_BUILDPACK_FILE:-""}
 cd src/*/integration
 
 echo "Run Uncached Buildpack"
-BUILDPACK_FILE="$UNCACHED_BUILDPACK_FILE" /
-  ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=120 --noisySkippings=false -- --cached=false
+BUILDPACK_FILE="$UNCACHED_BUILDPACK_FILE" \
+  ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=60 -- --cached=false
 
 echo "Run Cached Buildpack"
-BUILDPACK_FILE="$CACHED_BUILDPACK_FILE" /
-  ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=120 --noisySkippings=false -- --cached
+BUILDPACK_FILE="$CACHED_BUILDPACK_FILE" \
+  ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=60 -- --cached
