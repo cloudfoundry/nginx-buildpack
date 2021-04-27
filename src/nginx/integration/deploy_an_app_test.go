ee307e27e7efd43f35cf5c63ea5012e71276aabf
Timothy Hitchener <thitchener@vmware.com>
2021-04-27 15:04:17 +0000

    Remove 1.18 from integration tests, add 1.20
    Signed-off-by: Emily Johnson <emjohnson@vmware.com>

--- src/nginx/integration/deploy_an_app_test.go
+++ src/nginx/integration/deploy_an_app_test.go
@@ -113,7 +113,7 @@ var _ = Describe("CF Nginx Buildpack", func() {
 		It("Logs nginx buildpack version", func() {
 			PushAppAndConfirm(app)
 
-			Eventually(app.Stdout.String).Should(ContainSubstring(`Requested nginx version: stable => 1.18.`))
+			Eventually(app.Stdout.String).Should(ContainSubstring(`Requested nginx version: stable => 1.20.`))
 			Eventually(app.Stdout.String).Should(ContainSubstring(`Warning: usage of "stable" versions of NGINX is discouraged in most cases by the NGINX team.`))
 
 			Expect(app.GetBody("/")).To(ContainSubstring("Exciting Content"))
@@ -129,7 +129,7 @@ var _ = Describe("CF Nginx Buildpack", func() {
 		It("Logs nginx buildpack versions", func() {
 			Expect(app.Push()).ToNot(Succeed())
 
-			Eventually(app.Stdout.String).Should(ContainSubstring(`Available versions: mainline, stable, 1.18.x, 1.19.x`))
+			Eventually(app.Stdout.String).Should(ContainSubstring(`Available versions: mainline, stable, 1.19.x, 1.20.x`))
 		})
 	})
 
