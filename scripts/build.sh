cecc14e43e73560a4d6eb71a3aec76655767f8ac
Ryan Moran <rmoran@vmware.com>
2021-05-20 13:47:52 -0700

    Fix issue with building varify

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -x
 set -e
 set -u
 set -o pipefail
@@ -11,13 +12,18 @@ function main() {
   local src
   src="$(find "${ROOTDIR}/src" -mindepth 1 -maxdepth 1 -type d )"
 
-  for name in supply finalize; do
+  IFS=" " read -r -a binaries <<< "$(find "${src}" -name cli -type d -print0 | xargs -0)"
+
+  for path in "${binaries[@]}"; do
+    local name
+    name="$(basename "$(dirname "${path}")")"
+
     GOOS=linux \
       go build \
         -mod vendor \
         -ldflags="-s -w" \
         -o "${ROOTDIR}/bin/${name}" \
-          "${src}/${name}/cli"
+          "${path}"
   done
 }
 
