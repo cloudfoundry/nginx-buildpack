30b99f868e8948958f93faeda88cb9987386642e
Michael Eibensteiner <michael.eibensteiner@dynatrace.com>
2022-05-29 21:35:49 +0200

    Add dynatrace integration Enable hooks

--- src/nginx/finalize/cli/main.go
+++ src/nginx/finalize/cli/main.go
@@ -4,8 +4,10 @@ import (
 	"os"
 	"time"
 
-	"github.com/cloudfoundry/libbuildpack"
 	"github.com/cloudfoundry/nginx-buildpack/src/nginx/finalize"
+	_ "github.com/cloudfoundry/nginx-buildpack/src/nginx/hooks"
+
+	"github.com/cloudfoundry/libbuildpack"
 )
 
 func main() {
