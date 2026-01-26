e7464017b982e5dfa46ea1847ea1c51fe77a9433
i343759 <teodora.nikolova@sap.com>
2026-01-26 19:07:11 +0200

    fixed tests

--- src/nginx/integration/default_test.go
+++ src/nginx/integration/default_test.go
@@ -114,7 +114,7 @@ func testDefault(platform switchblade.Platform, fixtures string) func(*testing.T
 					Execute(name, filepath.Join(fixtures, "default", "unspecified_version"))
 				Expect(err).NotTo(HaveOccurred())
 
-				Eventually(logs).Should(ContainSubstring(`No nginx version specified - using mainline => 1.27.`))
+				Eventually(logs).Should(ContainSubstring(`No nginx version specified - using mainline => 1.28.`))
 				Eventually(logs).ShouldNot(ContainSubstring(`Requested nginx version:`))
 
 				Eventually(deployment).Should(Serve(ContainSubstring("Exciting Content")))
@@ -133,7 +133,7 @@ func testDefault(platform switchblade.Platform, fixtures string) func(*testing.T
 					Execute(name, filepath.Join(fixtures, "default", "mainline"))
 				Expect(err).NotTo(HaveOccurred())
 
-				Eventually(logs).Should(ContainSubstring(`Requested nginx version: mainline => 1.27.`))
+				Eventually(logs).Should(ContainSubstring(`Requested nginx version: mainline => 1.28.`))
 
 				Eventually(deployment).Should(Serve(ContainSubstring("Exciting Content")))
 
@@ -151,7 +151,7 @@ func testDefault(platform switchblade.Platform, fixtures string) func(*testing.T
 					Execute(name, filepath.Join(fixtures, "default", "stable"))
 				Expect(err).NotTo(HaveOccurred())
 
-				Eventually(logs).Should(ContainSubstring(`Requested nginx version: stable => 1.26.`))
+				Eventually(logs).Should(ContainSubstring(`Requested nginx version: stable => 1.29.`))
 				Eventually(logs).Should(ContainSubstring(`Warning: usage of "stable" versions of NGINX is discouraged in most cases by the NGINX team.`))
 
 				Eventually(deployment).Should(Serve(ContainSubstring("Exciting Content")))
@@ -170,7 +170,7 @@ func testDefault(platform switchblade.Platform, fixtures string) func(*testing.T
 					Execute(name, filepath.Join(fixtures, "default", "unavailable_version"))
 				Expect(err).To(HaveOccurred())
 
-				Eventually(logs).Should(ContainSubstring(`Available versions: mainline, stable, 1.26.x, 1.27.x`))
+				Eventually(logs).Should(ContainSubstring(`Available versions: mainline, stable, 1.28.x, 1.29.x`))
 			})
 		})
 
